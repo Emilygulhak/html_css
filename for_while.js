1b5e7b11be7feb2a290ef5af301f3b0a9e69d380
Emily Gulhak <118759000+Emilygulhak@users.noreply.github.com>
2023-09-04 21:21:51 -0300

    Update and rename index.html to for_while.js

--- for_while.js
+++ for_while.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <script>
 //Laços de repetição
 //São estruturas condicionais que repetem uma instrução até atingir determinada condição
